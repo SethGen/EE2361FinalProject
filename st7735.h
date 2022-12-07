c1d2368c9ba1780ab7f4c2726796d10b176b91dd
SethGen <115184573+SethGen@users.noreply.github.com>
2022-12-07 01:08:11 -0600

    Update st7735.h

--- st7735.h
+++ st7735.h
@@ -78,7 +78,12 @@ TFT 	PinNum	Pindesc
 #define LCD_RS 		LATAbits.LATA1	 // data or command
 #define LCD_CS 		LATAbits.LATA0	 // chip select
 #define LCD_SCK		LATBbits.LATB7	 // SPI Clock, for bit-bang SPI only
-#define	LCD_SDO		LATBbits.LATB6	 // SPI Data, for bit-bang SPI only				
+#define	LCD_SDO		LATBbits.LATB6	 // SPI Data, for bit-bang SPI only	
+
+
+//Menu Config
+#define menuCol colRed
+#define menuCol2 colBlue
 
 void setAddrWindow(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1);
 void ST7735_pushColor(unsigned int color); // for what?
@@ -87,6 +92,7 @@ void ST7735_fillScreen(unsigned int color);
 void ST7735_initB(void); // not used
 void ST7735_initR(void); // initialize a ST7735 chip
 void ST7735_drawString(unsigned char x, unsigned char y, char *c, unsigned int color, unsigned char size);
+void ST7735_drawStringH(unsigned char x, unsigned char y, char *c, unsigned int color, unsigned char size);
 void ST7735_drawChar(unsigned char x, unsigned char y, char c, unsigned int color, unsigned char size);
 void ST7735_fillCircle(unsigned char x0, unsigned char y0, unsigned char r, unsigned int color);
 void ST7735_drawCircle(unsigned char x0, unsigned char y0, unsigned char r, unsigned int color);
@@ -98,7 +104,8 @@ void ST7735_drawHorizontalLine(unsigned char x, unsigned char y, unsigned char l
 void ST7735_drawFastLine(unsigned char x, unsigned char y, unsigned char length, unsigned int color, unsigned char rotflag);
 void ST7735_drawLine(int x0, int y0, int x1, int y1, unsigned int color);
 unsigned char ST7735_getRotation();
-
+void menu(void);
+void plotData(unsigned char[]);
 void loadBitmapToLCD(char* filename);
 
 
